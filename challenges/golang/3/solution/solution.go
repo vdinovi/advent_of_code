7b2dacfdf6613fb0458753504c1a6b8cd01614f4
vdinovi <vito.dinovi@gmail.com>
2023-12-04 14:29:30 -0800

    12/03/23: init

---
+++ challenges/golang/3/solution/solution.go
@@ -0,0 +1,16 @@
+package solution
+
+import (
+	"io"
+)
+
+type Input io.Reader
+type Answer int
+
+func SolveP1(input Input) (Answer, error) {
+	return 0, nil
+}
+
+func SolveP2(input Input) (Answer, error) {
+	return 0, nil
+}
