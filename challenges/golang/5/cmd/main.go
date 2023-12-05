f76bd1ca3e607f064b5e764d80d40a5f8cf4e1fe
vdinovi <vito.dinovi@gmail.com>
2023-12-05 12:29:44 -0800

    12/05/23: part 2

--- challenges/golang/5/cmd/main.go
+++ challenges/golang/5/cmd/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	soln "github.com/vdinovi/advent_of_code_2023/challenges/golang/5/solution"
+)
+
 func main() {
 	// answer, err := soln.SolveP1(os.Stdin)
 	// if err != nil {
@@ -8,10 +15,10 @@ func main() {
 	// }
 	// fmt.Printf("Answer 1 = %d\n", answer)
 
-	// answer, err := soln.SolveP2(os.Stdin)
-	// if err != nil {
-	//	fmt.Fprintf(os.Stderr, "error: %s\n", err)
-	//	os.Exit(1)
-	// }
-	// fmt.Printf("Answer 2 = %d\n", answer)
+	answer, err := soln.SolveP2(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Answer 2 = %d\n", answer)
 }
--- challenges/golang/5/solution/solution.go
+++ challenges/golang/5/solution/solution.go
@@ -30,7 +30,37 @@ func SolveP1(input Input) (Answer, error) {
 }
 
 func SolveP2(input Input) (Answer, error) {
-	return 0, nil
+	sequences, mappings, err := scan(input)
+	if err != nil {
+		return 0, err
+	}
+	seeds, ok := sequences["seeds"]
+	if !ok {
+		return 0, fmt.Errorf("sequence 'seeds' not found")
+	}
+	mappings = invert(mappings)
+	minLoc := 0
+	// TODO: implement a non-brute force
+	maxLoc := 10834441
+	log := int(maxLoc / 100)
+	lowest := math.MaxInt64
+	for loc := minLoc; loc < maxLoc; loc += 1 {
+		if loc%log == 0 {
+			fmt.Printf("> %d%%\n", loc/log)
+		}
+		seed, err := traverse(loc, "location", "seed", mappings)
+		// fmt.Printf("location(%d) -> seed(%d)\n", loc, seed)
+		if err != nil {
+			return 0, err
+		}
+		for i := 0; i < len(seeds.elems); i += 2 {
+			if seed >= seeds.elems[i] && seed < seeds.elems[i]+seeds.elems[i+1] {
+				// fmt.Printf("lowest = min(%d, %d)\n", lowest, loc)
+				lowest = min(lowest, loc)
+			}
+		}
+	}
+	return Answer(lowest), nil
 }
 
 func traverse(value int, source, destination string, mappings map[string]*mapping) (int, error) {
@@ -39,10 +69,30 @@ func traverse(value int, source, destination string, mappings map[string]*mappin
 		if !ok {
 			return 0, fmt.Errorf("map not found for source %q", source)
 		}
-		fmt.Printf("%s(%d) -> %s(%d)\n", source, value, m.destination, m.get(value))
+		//fmt.Printf("%s(%d) -> %s(%d)\n", source, value, m.destination, m.get(value))
 		source, value = m.destination, m.get(value)
 	}
 	return value, nil
+}
+
+func invert(mappings map[string]*mapping) map[string]*mapping {
+	inverted := make(map[string]*mapping, len(mappings))
+	for _, m := range mappings {
+		inv := &mapping{
+			source:      m.destination,
+			destination: m.source,
+			trans:       make([][3]int, len(m.trans)),
+		}
+		for i := range m.trans {
+			inv.trans[i] = [3]int{
+				m.trans[i][1],
+				m.trans[i][0],
+				m.trans[i][2],
+			}
+		}
+		inverted[m.destination] = inv
+	}
+	return inverted
 }
 
 func scan(input Input) (map[string]*sequence, map[string]*mapping, error) {
