0a233bd189bd7db06317b12b08cc3ebdb3cf8aae
vdinovi <vito.dinovi@gmail.com>
2023-12-06 09:51:27 -0800

    12/06/23: part 1

---
+++ challenges/2023/golang/6/solution/solution.go
@@ -0,0 +1,47 @@
+package solution
+
+import "math"
+
+type Input struct {
+	Times     []int
+	Distances []int
+}
+type Answer int
+
+// distance(t, T) := t * (T - t) => Tt * t^2
+func distance(t, T int) int {
+	return t * (T - t)
+}
+
+// velocity(t, T) := d(distance(t, T))/dt = T * 2t
+func velocity(t, T int) int {
+	return T - 2*t
+}
+
+// optimal(T) := 0 = velocity(t, T) => (1/2)t
+func optimal(T float64) float64 {
+	return 0.5 * T
+}
+
+// solve set R = dist(t, T) and solve for t
+func roots(R, T float64) (float64, float64) {
+	discr := math.Sqrt(math.Pow(-T, 2) - 4*R)
+	r1 := (T - discr) / 2
+	r2 := (T + discr) / 2
+	return min(r1, r2), max(r1, r2)
+}
+
+func SolveP1(input Input) (Answer, error) {
+	product := 1
+	for i, T := range input.Times {
+		R := input.Distances[i]
+		r1, r2 := roots(float64(R), float64(T))
+		times := int(math.Floor(r2-0.01)) - int(math.Ceil(r1+0.01)) + 1
+		product *= times
+	}
+	return Answer(product), nil
+}
+
+func SolveP2(input Input) (Answer, error) {
+	return 0, nil
+}
---
+++ challenges/2023/golang/6/solution/solution_test.go
@@ -0,0 +1,83 @@
+package solution_test
+
+import (
+	_ "embed"
+	"testing"
+
+	soln "github.com/vdinovi/advent_of_code/challenges/2023/golang/6/solution"
+)
+
+//go:embed solution_test/1.txt
+var one_txt string
+
+func TestSolutionP1(t *testing.T) {
+	tests := []struct {
+		id       string
+		input    soln.Input
+		expected soln.Answer
+	}{
+		{
+			id: "Sample 1",
+			input: soln.Input{
+				Times:     []int{7, 15, 30},
+				Distances: []int{9, 40, 200},
+			},
+			expected: 0,
+		},
+		{
+			id: "Puzzle 1",
+			input: soln.Input{
+				Times:     []int{42, 89, 91, 89},
+				Distances: []int{308, 1170, 1291, 1467},
+			},
+			expected: 0,
+		},
+	}
+
+	for _, test := range tests {
+		answer, err := soln.SolveP1(test.input)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+			continue
+		}
+		if answer != test.expected {
+			t.Errorf("expected test %q to yield answer %v but got %v ", test.id, test.expected, answer)
+		}
+	}
+}
+
+func TestSolutionP2(t *testing.T) {
+	tests := []struct {
+		id       string
+		input    soln.Input
+		expected soln.Answer
+	}{
+		{
+			id: "Sample 1",
+			input: soln.Input{
+				Times:     []int{7, 15, 30},
+				Distances: []int{9, 40, 200},
+			},
+			expected: 0,
+		},
+		{
+			id: "Puzzle 1",
+			input: soln.Input{
+				Times:     []int{42, 89, 91, 89},
+				Distances: []int{308, 1170, 1291, 1467},
+			},
+			expected: 0,
+		},
+	}
+
+	for _, test := range tests {
+		answer, err := soln.SolveP2(test.input)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+			continue
+		}
+		if answer != test.expected {
+			t.Errorf("expected test %q to yield answer %v but got %v ", test.id, test.expected, answer)
+		}
+	}
+}
